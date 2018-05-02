ae779534b90ea5991c284d1ac83ced5c93766912
manavbot <mkorlipararules@gmail.com>
2018-05-02 08:06:47 -0700

    Update BattleshipDriver.java

--- BattleshipDriver.java
+++ BattleshipDriver.java
@@ -1,32 +1,42 @@
 import java.util.*;
 
 public class BattleshipDriver extends ShipSetup {
-	
+
 	public BattleshipDriver(int x, int y) {
 		super(x, y);
 		// TODO Auto-generated constructor stub
 	}
 
-	protected static int shipChoice;
-
-	public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	
+	public String game() {
 		System.out.println("Welcome to Battleship!");
 		System.out.println("The game will be played on an 8 by 8" + " field. 0-7 on the x-axis and 0-7 on the y-axis.");
 		System.out.println("Select how big your battleship is: 2,3,4?");
-		
-		Scanner scan = new Scanner(System.in);
-		shipChoice = scan.nextInt();
-		
-		if(shipChoice == 2) {
+		int size = scan.nextInt();
+		while(size != 2 || size != 3 || size != 4) {
+			System.out.println("Error! Ships can only be between sizes 2-4");
+			size = scan.nextInt();
+		}
+		System.out.println("Now pick coordinates to place your ship.");
+		if(size == 2) {
 			humanSetupFor2Ship();
+			computer();
 		}
-		else if(shipChoice == 3) {
+		else if(size == 3) {
 			humanSetupFor3Ship();
+			computer();
 		}
-		else if(shipChoice == 4) {
+		else if(size == 4) {
 			humanSetupFor4Ship();
+			computer();
 		}
-		
+		while()
+	}
+
+	protected static int shipChoice;
+
+	public static void main(String[] args) {
 	}
 
 }
