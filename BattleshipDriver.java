57aba74b17a2425bfb312726450248b2b96dafec
manavbot <mkorlipararules@gmail.com>
2018-05-07 06:55:00 -0700

    Update BattleshipDriver.java

--- BattleshipDriver.java
+++ BattleshipDriver.java
@@ -7,39 +7,74 @@ public class BattleshipDriver extends ShipSetup {
 		// TODO Auto-generated constructor stub
 	}
 
+	Random rand = new Random();
+
+
+
 	Scanner scan = new Scanner(System.in);
 
 	public void game() {
+		boolean isGame = false;
+		computer();
 		ArrayList<Integer> xPoints = new <Integer>ArrayList();
 		ArrayList<Integer> yPoints = new <Integer>ArrayList();
 		for (int i = 0; i > 8; i++) {
 			xPoints.add(i);
 			yPoints.add(i);
 		}
-		Ship ship = compArray.get(0);
-		Ship ship2 = compArray.get(1);
-		Ship ship3 = compArray.get(2);
+		Ship cship = compArray.get(0);
+		Ship cship2 = compArray.get(1);
+		Ship cship3 = compArray.get(2);
 		System.out.println("Welcome to Battleship!");
 		System.out.println("The game will be played on an 8 by 8" + " field. 0-7 on the x-axis and 0-7 on the y-axis.");
 		System.out.println("Select how big your battleship is: 2,3,4?");
 		int size = scan.nextInt();
-		while (size != 2 || size != 3 || size != 4) {
+		while (size != 2 && size != 3 && size != 4) {
+			
 			System.out.println("Error! Ships can only be between sizes 2-4");
 			size = scan.nextInt();
 		}
 		System.out.println("Now pick coordinates to place your ship.");
 		if (size == 2) {
 			humanSetupFor2Ship();
-			computer();
+			while (isGame == false) {
+				while (cship.isAlive() == true) {
+					System.out.println("Guess a point!");
+					System.out.println("x: ");
+					int xGuess = scan.nextInt();
+					System.out.println("y: ");
+					int yGuess = scan.nextInt();
+					System.out.println("broken");
+					if ((xGuess == compX.get(0) && yGuess == compY.get(0))) {
+						Coordinate c = new Coordinate(compX.get(0), compY.get(0));
+						System.out.println("Hit!");
+						c.setDamage(true);
+					} else if (xGuess == compX.get(1) && yGuess == compY.get(2)) {
+						Coordinate c = new Coordinate(compX.get(1), compY.get(1));
+						System.out.println("Hit!");
+						c.setDamage(true);
+					} else {
+						System.out.println("You missed");
+					}
+					int compGuessX = rand.nextInt(8);
+					int compGuessY = rand.nextInt(8);
+					if (compGuessX == humanX.get(0) && compGuessY == humanY.get(0)) {
+						Coordinate c = new Coordinate(humanX.get(0), humanY.get(0));
+						System.out.println("The computer has got a hit!");
+						c.setDamage(true);
+					} else if (compGuessX == humanX.get(1) && compGuessY == humanY.get(2)) {
+						Coordinate c = new Coordinate(humanX.get(1), humanY.get(1));
+						System.out.println("The computer has got a hit!");
+						c.setDamage(true);
+					}
+				}
+			}
 		} else if (size == 3) {
 			humanSetupFor3Ship();
-			computer();
 		} else if (size == 4) {
-			humanSetupFor4Ship(); 
+			humanSetupFor4Ship();
 		}
-		while (ship.isAlive() == true) {
-			
-		}
+
 	}
 
 	protected static int shipChoice;
@@ -81,6 +116,8 @@ public class BattleshipDriver extends ShipSetup {
 	}
 
 	public static void main(String[] args) {
+		BattleshipDriver b = new BattleshipDriver(0, 0);
+		b.game();
 	}
 
 }
