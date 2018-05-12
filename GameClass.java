862a2b6c3a7e9845f8153609b138fcc33559b24c
manavbot <mkorlipararules@gmail.com>
2018-05-12 11:06:27 -0700

    Update GameClass.java

--- GameClass.java
+++ GameClass.java
@@ -1,3 +1,4 @@
+
 import java.util.*;
 
 public class GameClass extends ShipSetup {
@@ -5,6 +6,17 @@ public class GameClass extends ShipSetup {
 	public static ArrayList<Integer> compGuessesX = new <Integer>ArrayList(); // NEW
 	public static ArrayList<Integer> compGuessesY = new <Integer>ArrayList(); // NEW
 	public static boolean guesses[][] = new boolean[8][8];
+	public static boolean humanBoard[][] = new boolean[8][8];
+	public static boolean compBoard[][] = new boolean[8][8];
+	public static ArrayList<Boolean> living = new ArrayList<Boolean>(3);
+	public static ArrayList<Boolean> dead = new ArrayList<Boolean>(3);
+	public static ArrayList<Boolean> damaged = new ArrayList<Boolean>(3);
+	public static ArrayList<Boolean> hLiving = new ArrayList<Boolean>(3);
+	public static ArrayList<Boolean> hDead = new ArrayList<Boolean>(3);
+	public static ArrayList<Boolean> hDamaged = new ArrayList<Boolean>(3);
+	public static ArrayList<Coordinate> compGuess = new <Coordinate>ArrayList();
+	public static ArrayList<Integer> flips = new <Integer>ArrayList();
+	public static ArrayList<Integer> flips2 = new <Integer>ArrayList(); // NEW
 
 	public GameClass(int x, int y) {
 		super(x, y);
@@ -16,11 +28,9 @@ public class GameClass extends ShipSetup {
 	Scanner scan = new Scanner(System.in);
 
 	public void game() {
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
-				guesses[i][j] = false;
-			}
-		}
+		/*
+		 * for (int k = 0; k < 3; k++) { living.set(k, true); }
+		 */
 		boolean isGame = false;
 		computer();
 		ArrayList<Integer> xPoints = new <Integer>ArrayList();
@@ -80,13 +90,16 @@ public class GameClass extends ShipSetup {
 		if (size3 == 4) {
 			humanSetupFor4Ship();
 		}
-		for (int i = 0; i < compX.size(); i++) {
-			System.out.println(compX.get(i) + "," + compY.get(i));
-		}
-		System.out.println();
-		for (int i = 0; i < humanX.size(); i++) {
-			System.out.println(humanX.get(i) + "," + humanY.get(i));
-		}
+		for (int i = 0; i < compX.size(); i++) {// FOR TESTING PURPOSES ONLY, REMOVE FOR ACTUAL PRESENTATION
+			System.out.println(compX.get(i) + "," + compY.get(i));// FOR TESTING PURPOSES ONLY, REMOVE FOR ACTUAL
+																	// PRESENTATION
+		} // FOR TESTING PURPOSES ONLY, REMOVE FOR ACTUAL PRESENTATION
+		System.out.println();// FOR TESTING PURPOSES ONLY, REMOVE FOR ACTUAL PRESENTATION
+		for (int i = 0; i < humanX.size(); i++) {// FOR TESTING PURPOSES ONLY, REMOVE FOR ACTUAL PRESENTATION
+			System.out.println(humanX.get(i) + "," + humanY.get(i));// FOR TESTING PURPOSES ONLY, REMOVE FOR ACTUAL
+																	// PRESENTATION
+		} // FOR TESTING PURPOSES ONLY, REMOVE FOR ACTUAL PRESENTATION
+		int compRound = 0;
 		while (isGame == false) {
 			int round = 1;
 			Ship humShip1 = humArray.get(0);
@@ -95,96 +108,322 @@ public class GameClass extends ShipSetup {
 			/**
 			 * Human guessing portion
 			 */
-			System.out.println("Guess a point!");
-			System.out.println("x: ");
-			int xGuess = scan.nextInt();
-			System.out.println("y: ");
-			int yGuess = scan.nextInt();
-			// goes through the x's and y's to check if the coordinates were guessed
-			// beforehand
-			while (guesses[xGuess][yGuess] == true) {
-				System.out.println("You already guessed that point, guess again.");
+			try {
+				System.out.println("Guess a point!");
 				System.out.println("x: ");
-				xGuess = scan.nextInt();
+				int xGuess = scan.nextInt();
 				System.out.println("y: ");
-				yGuess = scan.nextInt();
-			}
-			guesses[xGuess][yGuess] = true;
-			// checks the humans guess
-			boolean hit = false;
-			for (int i = 0; i < compX.size(); i++) {
-				if ((xGuess == compX.get(i) && yGuess == compY.get(i))) {
-					Coordinate c = new Coordinate(compX.get(i), compY.get(i));
-					System.out.println("Hit!");
-					c.setDamage(true);
-					hit = true;
-					break;
-				} else {
-					hit = false;
+				int yGuess = scan.nextInt();
+				// goes through the x's and y's to check if the coordinates were guessed
+				// beforehand
+				while (guesses[xGuess][yGuess] == true) {
+					System.out.println("You already guessed that point, guess again.");
+					System.out.println("x: ");
+					xGuess = scan.nextInt();
+					System.out.println("y: ");
+					yGuess = scan.nextInt();
 				}
-			}
-			if (hit == false) {
-				System.out.println("You missed!");
+				guesses[xGuess][yGuess] = true;
+				// checks the humans guess
+				boolean hit = false;
+				for (int i = 0; i < compX.size(); i++) {
+					if ((xGuess == compX.get(i) && yGuess == compY.get(i))) {
+						Coordinate c = new Coordinate(compX.get(i), compY.get(i));
+						System.out.println("Hit!");
+						humanBoard[xGuess][yGuess] = true;
+						c.setDamage(true);
+						hit = true;
+						for (int m = 0; m < compArray.size(); m++) {
+							if ((compArray.get(m).getSize() == 2)) {
+								TwoLengthShip currentShip = (TwoLengthShip) compArray.get(m);
+								int x1 = currentShip.getC1().getX();
+								int y1 = currentShip.getC1().getY();
+								int x2 = currentShip.getC2().getX();
+								int y2 = currentShip.getC2().getY();
+
+								if (humanBoard[x1][y1] && humanBoard[x2][y2]) {
+									compArray.get(m).setStatus(false);
+									// living.set(m, false);
+									// dead.set(m, true);
+									// damaged.set(m, true);
+									System.out.println("You sunk a ship of length two!");
+									// System.out.println();
+									System.out.println("Your stats:");
+									System.out.println(updateStatus(0));
+									System.out.println("Computer's stats:");
+									System.out.println(updateCompStatus(0));
+									compArray.remove(m);
+									break;
+								} else if (humanBoard[x1][y1] || humanBoard[x2][y2]) {
+									compArray.get(m).setDamage(true);
+									// living.set(m, true);
+									// dead.set(m, false);
+									// damaged.set(m, true);
+									currentShip.setDamage(true);
+									break;
+								}
+							} else if ((compArray.get(m).getSize() == 3)) {
+								ThreeLengthShip currentShip = (ThreeLengthShip) compArray.get(m);
+								int x1 = currentShip.getC1().getX();
+								int y1 = currentShip.getC1().getY();
+								int x2 = currentShip.getC2().getX();
+								int y2 = currentShip.getC2().getY();
+								int x3 = currentShip.getC3().getX();
+								int y3 = currentShip.getC3().getY();
+
+								if (humanBoard[x1][y1] && humanBoard[x2][y2] && humanBoard[x3][y3]) {
+									currentShip.setStatus(false);
+									// living.set(m, false);
+									// dead.set(m, true);
+									// damaged.set(m, true);
+									currentShip.setStatus(false);
+									System.out.println("You sunk a ship of length three!");
+									// System.out.println();
+									System.out.println("Your stats:");
+									System.out.println(updateStatus(0));
+									System.out.println("Computer's stats:");
+									System.out.println(updateCompStatus(0));
+									compArray.remove(m);
+									break;
+								} else if (humanBoard[x1][y1] || humanBoard[x2][y2] || humanBoard[x3][y3]) {
+									// living.set(m, true);
+									// dead.set(m, false);
+									// damaged.set(m, true);
+									currentShip.setDamage(true);
+									break;
+								}
+							} else if ((compArray.get(m).getSize() == 4)) {
+								FourLengthShip currentShip = (FourLengthShip) compArray.get(m);
+								int x1 = currentShip.getC1().getX();
+								int y1 = currentShip.getC1().getY();
+								int x2 = currentShip.getC2().getX();
+								int y2 = currentShip.getC2().getY();
+								int x3 = currentShip.getC3().getX();
+								int y3 = currentShip.getC3().getY();
+								int x4 = currentShip.getC4().getX();
+								int y4 = currentShip.getC4().getY();
+
+								if (humanBoard[x1][y1] && humanBoard[x2][y2] && humanBoard[x3][y3]
+										&& humanBoard[x4][y4]) {
+									// living.set(m, false);
+									// dead.set(m, true);
+									// damaged.set(m, true);
+									currentShip.setStatus(false);
+									System.out.println("You sunk a ship of length four!");
+									// System.out.println();
+									System.out.println("Your stats:");
+									System.out.println(updateStatus(0));
+									System.out.println("Computer's stats:");
+									System.out.println(updateCompStatus(0));
+									compArray.remove(m);
+									break;
+								} else if (humanBoard[x1][y1] || humanBoard[x2][y2] || humanBoard[x3][y3]
+										|| humanBoard[x4][y4]) {
+									// living.set(m, true);
+									// dead.set(m, false);
+									// damaged.set(m, true);
+									currentShip.setDamage(true);
+									break;
+								}
+							}
+						}
+						break;
+					} else {
+						hit = false;
+					}
+				}
+				if (hit == false) {
+					System.out.println("You missed!");
+				}
+			} catch (ArrayIndexOutOfBoundsException a) {
+				System.out.println("Your guess was outside of the field. Please make another guess.");
 			}
 			/**
 			 * Computer guessing portion
 			 */
 			int compGuessX = rand.nextInt(8);
 			int compGuessY = rand.nextInt(8);
-			for (int i = 0; i < round - 1; i++) {
-				while (compGuessesX.get(i) == compGuessX && compGuessesY.get(i) == compGuessY) {
-					System.out.println("You already guesses this point, pick again");
+			while (compBoard[compGuessX][compGuessY] == true) {
+				System.out.println("Duplicate");
+				compGuessX = rand.nextInt(8);
+				compGuessY = rand.nextInt(8);
+
+			}
+			compBoard[compGuessX][compGuessY] = true;
+			int count = 0;
+			if (compGuess.size() > 0) {
+				Coordinate nextGuess = compGuess.get(compRound);
+				Random coin = new Random();
+				int flip = coin.nextInt(2);
+				flips.add(flip);
+				if (compRound > 0) {
+					if (flips.get(0) == 1) {
+						flip = 0;
+					} else if (flips.get(0) == 0) {
+						flip = 1;
+					}
 				}
+				if (flip == 0) {
+					int flip2 = coin.nextInt(2);
+					flips2.add(flip2);
+					System.out.println(compRound);
+					if (compRound > 0) {
+						if (flips2.get(0) == 1) {
+							flip2 = 0;
+						} else if (flips2.get(1) == 0) {
+							flip2 = 1;
+						}
+					}
+					if (flip2 == 0) {
+						int x = nextGuess.getX() + 1;
+						System.out.println(x);
+						if (x > 7) {
+							x = nextGuess.getX() - 1;
+						}
+						nextGuess = new Coordinate(x, nextGuess.getY());
+						compGuess.add(nextGuess);
+						compGuessX = x;
+						compGuessY = nextGuess.getY();
+						compRound++;
+					} else {
+						int x = nextGuess.getX() - 1;
+						System.out.println(x);
+						if (x < 0) {
+							x = nextGuess.getX() + 1;
+						}
+						nextGuess = new Coordinate(x, nextGuess.getY());
+						compGuess.add(nextGuess);
+						compGuessX = x;
+						compGuessY = nextGuess.getY();
+						compRound++;
+					}
+				} else {
+					int flip2 = coin.nextInt(2);
+					flips2.add(flip2);
+					System.out.println(compRound);
+					if (compRound > 0) {
+						if (flips2.get(0) == 1) {
+							flip2 = 0;
+						} else if (flips2.get(1) == 0) {
+							flip2 = 1;
+						}
+					}
+					if (flip == 0) {
+						int y = nextGuess.getY() + 1;
+						if (y > 7) {
+							y = nextGuess.getY() - 1;
+						}
+						nextGuess = new Coordinate(nextGuess.getX(), y);
+						compGuess.add(nextGuess);
+						compGuessX = nextGuess.getX();
+						compGuessY = nextGuess.getY();
+						compRound++;
+					} else {
+						int y = nextGuess.getY() - 1;
+						if (y < 0) {
+							y = nextGuess.getY() + 1;
+						}
+						nextGuess = new Coordinate(nextGuess.getX(), y);
+						compGuess.add(nextGuess);
+						compGuessX = nextGuess.getX();
+						compGuessY = y;
+						compRound++;
+					}
+				}
+				// }
+				// flips.add(flip);
+				// }
 			}
 
 			if (compGuessX == humanX.get(0) && compGuessY == humanY.get(0)) {
+				System.out.println("The computer guessed point: " + compGuessX + ", " + compGuessY);
 				Coordinate c = new Coordinate(humanX.get(0), humanY.get(0));
 				System.out.println("The computer has got a hit!");
 				c.setDamage(true);
-			} else if (compGuessX == humanX.get(1) && compGuessY == humanY.get(2)) {
+				compGuess.add(c);
+			} else if (compGuessX == humanX.get(1) && compGuessY == humanY.get(1)) {
+				System.out.println("The computer guessed point: " + compGuessX + ", " + compGuessY);
 				Coordinate c = new Coordinate(humanX.get(1), humanY.get(1));
 				System.out.println("The computer has got a hit!");
 				c.setDamage(true);
+				compGuess.add(c);
+			} else if (compGuessX == humanX.get(2) && compGuessY == humanY.get(2)) {
+				System.out.println("The computer guessed point: " + compGuessX + ", " + compGuessY);
+				Coordinate c = new Coordinate(humanX.get(1), humanY.get(1));
+				System.out.println("The computer has got a hit!");
+				c.setDamage(true);
+				compGuess.add(c);
+			} else {
+				System.out.println("The computer guessed point: " + compGuessX + ", " + compGuessY + " and missed");
 			}
-			round++;
 		}
+
 	}
 
 	protected static int shipChoice;
 
 	// NEW METHOD
-	public String updateStatus(ArrayList<Ship> arr, int low, int high) {
-		int damageCount = 0;
-		int aliveCount = 0;
-		int deadCount = 0;
-		low = 0;
-		high = arr.size() - 1;
-		String s = "";
-		if (low > high) {
+	int damageCount = 0;
+	int aliveCount = 0;
+	int deadCount = 0;
+	String s = "";
+
+	private String updateStatus(int low) {
+
+		if (low == 3) {
 			s += "Alive: " + aliveCount + "\n" + "Dead: " + deadCount + "\n" + "Damaged: " + damageCount + "\n";
+			damageCount = 0;
+			aliveCount = 0;
+			deadCount = 0;
 			return s;
+
 		} else {
-			if (arr.get(low).isAlive()) {
+			s = "";
+			if (humArray.get(low).isAlive()) {
 				aliveCount++;
 			}
-			if (arr.get(low).isDamaged()) {
+			if (humArray.get(low).isDamaged()) {
 				damageCount++;
 			}
-			if (!arr.get(low).isDamaged()) {
-				deadCount++;
-			}
-			if (arr.get(high).isAlive()) {
-				aliveCount++;
-			}
-			if (arr.get(high).isDamaged()) {
-				damageCount++;
-			}
-			if (!arr.get(high).isDamaged()) {
+			if (!humArray.get(low).isAlive()) {
 				deadCount++;
 			}
 
-			return updateStatus(arr, ++low, --high);
 		}
+		low = low + 1;
+		return updateStatus(low);
+
+	}
+
+	int damageCount2 = 0;
+	int aliveCount2 = 0;
+	int deadCount2 = 0;
+	String s2 = "";
+
+	private String updateCompStatus(int low) {
+
+		if (low == 3) {
+			s += "Alive: " + aliveCount2 + "\n" + "Dead: " + deadCount2 + "\n" + "Damaged: " + damageCount2 + "\n";
+			damageCount2 = 0;
+			aliveCount2 = 0;
+			deadCount2 = 0;
+			return s2;
+
+		} else {
+			s = "";
+			if (compArray.get(low).isAlive()) {
+				aliveCount2++;
+			}
+			if (compArray.get(low).isDamaged()) {
+				damageCount2++;
+			}
+			if (!compArray.get(low).isAlive()) {
+				deadCount2++;
+			}
+
+		}
+		low = low + 1;
+		return updateCompStatus(low);
 
 	}
 
