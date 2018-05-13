57ce32ab9991e57abdadf420168b266399d0c7cc
khanhthyle <30767998+khanhthyle@users.noreply.github.com>
2018-05-12 17:47:25 -0700

    Updated TwoLengthShip w/ Javadoc

--- TwoLengthShip.java
+++ TwoLengthShip.java
@@ -1,55 +1,96 @@
 public class TwoLengthShip extends Ship {
 	Coordinate c1;
 	Coordinate c2;
-	
-	
-	public TwoLengthShip(Coordinate c1, Coordinate c2){
+
+	/**
+	 * Creates a two length ship with two coordinates
+	 * 
+	 * @param c1
+	 *            - first coordinate
+	 * @param c2
+	 *            - second coordinate
+	 */
+	public TwoLengthShip(Coordinate c1, Coordinate c2) {
 		this.c1 = c1;
 		this.c2 = c2;
 	}
+
 	@Override
+	/**
+	 * Boolean to check if ship is damaged
+	 * 
+	 * @return true if a coordinate has been guessed (ship is damaged), false if
+	 *         not
+	 */
 	public boolean isDamaged() {
-		if((c1.isDamaged() || c2.isDamaged())) {
+		if ((c1.isDamaged() || c2.isDamaged())) {
 			return true;
-		}
-		else if(c1.isDamaged() && c2.isDamaged()) {
+		} else if (c1.isDamaged() && c2.isDamaged()) {
 			setStatus(false);
 			return true;
-		}
-		else{
+		} else {
 			return false;
 		}
 	}
 
 	@Override
+	/**
+	 * Boolean to check if ship is alive
+	 * 
+	 * @return true if all coordinates have not been guessed, false if not
+	 */
 	public boolean isAlive() {
-		if(c1.isDamaged() || c2.isDamaged()) {
+		if (c1.isDamaged() || c2.isDamaged()) {
 			return true;
-		}
-		else if(!c1.isDamaged() && !c2.isDamaged()) {
+		} else if (!c1.isDamaged() && !c2.isDamaged()) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
+
+	/**
+	 * Getter method for ship size
+	 * 
+	 * @return ship size
+	 */
 	public int getSize() {
 		return 2;
 	}
+
+	/**
+	 * Getter method for first coordinate
+	 * 
+	 * @return c1 - first coordinate
+	 */
 	public Coordinate getC1() {
 		return c1;
 	}
+
+	/**
+	 * Getter method for second coordinate
+	 * 
+	 * @return c2 - second coordinate
+	 */
 	public Coordinate getC2() {
 		return c2;
 	}
+
 	@Override
+	/**
+	 * Sets status for ship
+	 */
 	public void setStatus(boolean status) {
 		isAlive = status;
-		
+
 	}
+
 	@Override
+	/**
+	 * Sets damage for ship
+	 */
 	public void setDamage(boolean status) {
 		isDamaged = status;
-		
+
 	}
 }
